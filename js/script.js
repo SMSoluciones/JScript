e2b5bd704b4dbd277534e66f0e7e5b2f76a385d5
Sebastian Munoz <smunoz.id@gmail.com>
2022-07-04 21:53:15 -0300

    DESAFIO COMPLEMENTARIO 4

--- js/script.js
+++ js/script.js
@@ -4,11 +4,25 @@
 
 registroLogin = [];
 
+const registroUsuarios = () => {
+  localStorage.setItem("usuariosIngresados", JSON.stringify(registroLogin));
+};
+
 const usuarioIngresado = () => {
   ingreso.style.display = "none";
-  login.innerHTML = `<h2>¡Bienvenido al sistema ${nombreDeUsuario} ${apellidoDeUsuario} su legajo es ${legajoDeUsuario}!</h2>`;
 
-  registroLogin.push(legajoDeUsuario);
+  if (
+    nombreDeUsuario == "" &&
+    apellidoDeUsuario == "" &&
+    legajoDeUsuario == ""
+  ) {
+    alert("Porfavor ingrese algun dato. Ingreso como invitado");
+    login.innerHTML = `<h2>¡Bienvenido Invitado!</h2>`;
+  } else {
+    login.innerHTML = `<h2>¡Bienvenido al sistema ${nombreDeUsuario} ${apellidoDeUsuario} su legajo es ${legajoDeUsuario}!</h2>`;
+    registroLogin.push(legajoDeUsuario);
+  }
+  registroUsuarios();
 };
 
 // Variables Storage
@@ -39,16 +53,18 @@ formulario.addEventListener("submit", (e) => {
   usuarioIngresado();
 });
 
-if (!!nombreDeUsuario && !!apellidoDeUsuario && !!legajoDeUsuario) {
+// Operador AND >>>> ENTREGA OPTIMIZANDO
+!!nombreDeUsuario &&
+  !!apellidoDeUsuario &&
+  !!legajoDeUsuario &&
   usuarioIngresado();
-}
 
 // Lista de Productos.
 
 const modelos = [
   {
     id: 1,
-    modelo: "ISIDRO",
+    modelo: "New ISIDRO",
     chasis: "Mercedes Benz O500",
     image: "Imagen1",
   },
@@ -82,9 +98,17 @@ const modelos = [
 
 const unidadCargada = [];
 
+// Desestructuracion de Arrays >>>> ENTREGA OPTIMIZANDO
+
+const [a, b, c, d, e] = modelos;
+
 // Seleccion de contenedor
 const contenedorModelos = document.getElementById("modelo");
 const chasisCargados = document.getElementById("chasisCargados");
+const botonContinuar = document.querySelector(".botonSeguir");
+const masVendido = document.querySelector(".masVendido");
+
+masVendido.innerHTML = `El modelo mas vendido es:<b> ${a.modelo} ${a.chasis} </b>`;
 
 // Insercion de Lista
 
@@ -119,7 +143,12 @@ const colocarModelo = (producto) => {
   </div>`;
 
   boxModelos.append(contenedor);
-  // Product Storage
+
+  // Esconder botonera //>>>> ENTREGA OPTIMIZANDO
+  unidadCargada.length <= 1
+    ? (contenedorModelos.style.display = "none") &&
+      (botonContinuar.style.display = "flex")
+    : (botonContinuar.style.display = "none"); // Product Storage
   localStorage.setItem("unidadesCargadas", JSON.stringify(producto));
   const traerJSON = localStorage.getItem("unidadesCargadas");
   // console.log(traerJSON); // Me devuelve un String.
